b38a128fbe6113bfc4a152206dbceeea9f3a6450
nomzy-kush <nomzykush@gmail.com>
2021-07-24 03:21:24 -0700

    first commit

---
+++ src/components/Error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div
+        className="showcase"
+        style={{
+        height: '650px',
+        display: 'flex', 
+        alignItems: 'center',
+        justifyContent: 'center'
+         }}
+        >
+            <h1>Looks like this page doesn't exist</h1>
+        </div>
+    )
+}
+
+export default Error
